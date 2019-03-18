dc9cf934819c572ac029531a1559610f53fadf8c
Amit <amitzmn@gmail.com>
2019-03-17 22:12:29 -0400

    Added Footer

---
+++ resources/views/inc/greeting.blade.php
@@ -0,0 +1,6 @@
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Welcome To The Test Project</h1>
+    <p class="lead">Laravel & Vue</p>
+  </div>
+</div>
