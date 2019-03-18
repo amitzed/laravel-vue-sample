dd2f0a974e98029ff56f44a19043b3bb42a1cbbf
Amit <amitzmn@gmail.com>
2019-03-17 21:07:57 -0400

    Added Nav Bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,11 @@
   <head>
     <meta charset="utf-8">
     <title>Test Project</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
+    @include('inc.navbar')
+
     @yield('content')
     @include('inc.sidebar')
   </body>
